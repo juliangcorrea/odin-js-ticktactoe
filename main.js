5caac0e176a30bdcba819c9f78c4c1041539fe7b
Julian Correa <juliangcorrea@hotmail.com>
2025-01-10 16:13:44 -0300

    Add full game logic and basic html structure. Note: Logic need checking

--- main.js
+++ main.js
@@ -1,59 +1,37 @@
 const Gameboard = {
-    gameboard: [],
+    gameboard: {},
     players: {},
-    gameflow: []
+    gameflow: {}
 }
+
 
 // Logic sketch
+const allMoves = ['l1', 'l2', 'l3', 'm1', 'm2', 'm3', 'r1', 'r2', 'r3']
+const gridSize = 3
 
-const test = ["l1", "m1", "r1", "l2", "m2", "r2", "l3", "m3", "r3"]
-
-const player1Moves = ['l1', 'm2', 'm3', 'l2', 'r2']
-const player2Moves = ['l3', 'r3', 'm1', 'r1']
-
-function sortMoves(playerMoves){
-  const sortedMoves = playerMoves.sort()
-  return sortedMoves
-}
-
-function defWin1(moves){
-  moves = sortMoves(moves)
-  if(moves.length > 0){
-    let temp1 = moves.filter(item => item[0] == moves[0][0])
-    if(temp1.length == 3) {
-      return true
-  } else {
-    defWin1(moves.filter(item => !temp1.includes(item)))
-  }
-  }
-  return false
-}
-
-function defWin2(moves){
-  moves = sortMoves(moves)
-  if(moves.length > 0){
-    let temp1 = moves.filter(item => item[1] == moves[0][1])
-    if(temp1.length == 3) {
-      return true
-  } else {
-    defWin2(moves.filter(item => !temp1.includes(item)))
-  }
-  }
-  return false
-}
-
-function defWin3(moves){
-  moves = sortMoves(moves)
-  let test1 = moves.map(item => item[1])
-  if(test1.includes('1') && test1.includes('2') && test1.includes('3')){
+function checkWin(moves, limitStart = -1 - gridSize){
+  moves = moves.sort()
+  //Chech for diagonal win
+  const checkLeftSide = moves.filter(item => item[1] == 1)
+  const checkRightSide = moves.filter(item => item[1] == 3)
+  if(moves.includes('m2') && checkLeftSide.map(item => item[0]).includes(checkRightSide.map(item => item[0])[0]) > 0){
     return true
   }
-  return false
-}
-
-function checkWin(moves){
-  if(defWin1(moves) || defWin2(moves) || defWin3(moves)){
-    return true
+  //until all tiles have been selected check for row or column win
+  if(moves.length > 0){
+    const checkWinCol = moves.filter(item => item[0] == moves[0][0])
+    const checkWinRow = moves.filter(item => item[1] == moves[0][1])
+    if(checkWinCol.length == 3 || checkWinRow.length == 3) {
+      return true
+    } else {
+      /* Once done the first check, uses the upper-left to bottom-right diagonal as 
+        guide (using the total grid size as meassure) to leave out rows and columns 
+        to call fucntion recursively*/
+      const newLimit = limitStart + gridSize + 1 //calculates the next diagonal tile
+      const limitMove = allMoves[newLimit] //gets the value of the tile
+      const newMoves = moves.filter(item => !((item[0] == limitMove[0] && item[1] <= limitMove[1]) || item[1] <= limitMove[1]))
+      return checkWin(newMoves, newLimit)
+    }
   }
   return false
 }
@@ -70,7 +48,5 @@ function Game(player1, player2){
   } else {
     return error
   }
-  
 }
 
-Game(player1Moves, player2Moves)
